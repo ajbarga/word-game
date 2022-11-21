b471d9d7d246f80f9174dc094d9fd2fbf31c40f9
ajbarga <alex.barga@philips.com>
2022-11-21 18:26:53 -0500

    Re-serve in vite!

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 // © 2022 Alex Barga. All rights reserved.
 // Reproduction or transmission in whole or in part, in any form or by any means, electronic,
 // mechanical or otherwise, is prohibited without the prior  written consent of the owner.
-import React from 'react';
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-
-import App from './App';
+import App from './App'
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container)
 
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+  );
